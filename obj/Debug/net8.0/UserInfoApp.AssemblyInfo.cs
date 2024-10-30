47b227496c2bf43fa9f7f024d2f9de73c5d341ac
Sergei Voronin <s.voronin@inlu.net>
2024-10-30 22:12:45 +0300

    Add gitignore, readme.

--- obj/Debug/net8.0/UserInfoApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/UserInfoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserInfoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cdb4e9b7d8a06fc90819ce3809b1c63f6d1ee72")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserInfoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserInfoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
